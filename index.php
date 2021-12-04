f98915deaf6d7df182f5a4e068b55c2d5e2d5560
Luis <luis.valencia@est.iudigital.edu.co>
2021-12-03 19:21:13 -0500

    Ajustes de facturacion y sesion
    Se ajusta el formato de la factura y algunos detalles de la sesión

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 <?php
 require_once('mysql.php');
+
 session_start();
+
 ?>
 
 <!DOCTYPE html>
@@ -19,7 +21,7 @@ session_start();
 
 	<link rel="canonical" href="https://demo-basic.adminkit.io/" />
 
-	<title>Autos  Colombia Demo - Bootstrap 5 Admin Template</title>
+	<title>Autos Colombia Demo - Bootstrap 5 Admin Template</title>
 
 	<link href="css/app.css" rel="stylesheet">
 	<link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;600&display=swap" rel="stylesheet">
@@ -77,8 +79,8 @@ session_start();
 				<div class="sidebar-cta">
 					<div class="sidebar-cta-content">
 						<strong class="d-inline-block mb-2"></strong>
-						
-						
+
+
 					</div>
 				</div>
 			</div>
@@ -102,7 +104,7 @@ session_start();
 								</a>
 
 								<a class="nav-link dropdown-toggle d-none d-sm-inline-block" href="#" data-bs-toggle="dropdown">
-									<img src="img/avatars/avatar.jpg" class="avatar img-fluid rounded me-1" alt="Charles Hall" /> <span class="text-dark">asd Hall</span>
+									<img src="img\avatars\avatar.png" class="avatar img-fluid rounded me-1" alt="<IUDigital" /> <span class="text-dark">Bienvenido!Tu sesion esta activa</span>
 								</a>
 
 
@@ -121,11 +123,11 @@ session_start();
 						} else {
 						?>
 							<li class="nav-item dropdown me-2">
-								<a href="pages-sign-in.php" class="list-group-item">Sing In</a>
+								<a href="pages-sign-in.php" class="list-group-item">Inicio Sesion</a>
 							</li>
 
 							<li class="nav-item dropdown">
-								<a href="pages-sign-up.php" class="list-group-item">Sing Up</a>
+								<a href="pages-sign-up.php" class="list-group-item">Registro</a>
 							</li>
 						<?php
 						}
@@ -164,50 +166,71 @@ session_start();
 							// foreach ($results->fetch_row() as $row) {
 							//   echo $row;
 							// }
+							if (isset($_SESSION['user_email'])) {
 
 
-							$sql = "SELECT * FROM celdas";
-							$stmt = $conn->prepare($sql);
-							$stmt->execute();
-							$result = $stmt->get_result();
-							while ($row = $result->fetch_assoc()) {
 
-								echo   '<div class="col">';
-								echo    '<div class="card shadow-sm">';
-								echo           '<h3>'. $row['tipovehiculo'] . '</h3>';
-								if($row['estado']==0){
-								echo     '<img src="https://firebasestorage.googleapis.com/v0/b/datos-ceb65.appspot.com/o/New%20Project%20(6).png?alt=media&token=8e603bf2-0532-4bb8-8b6b-8cb7978b20c3" class="rounded float-start" alt="...">';
-								echo     ' <div class="card-body">';								
-								echo       '<div class="d-flex justify-content-between align-items-center">';
-								echo         '<div class="btn-group">';
-								echo 			'<a class="btn btn-primary me-2" href="pages-registro-celda.php?id='.$row['id'].'">Asignar </a>';
+								$sql = "SELECT * FROM celdas";
+								$stmt = $conn->prepare($sql);
+								$stmt->execute();
+								$result = $stmt->get_result();
+								while ($row = $result->fetch_assoc()) {
 
-								
-								echo          '<h6>CELDA#'. $row['id'] . '</h6>';
-								echo       '</div>';
-								echo        '<small class="text-muted"></small>';
-								echo      '</div>';
-								echo     '</div>';
-								echo   '</div>';
-								echo '</div>';
+									echo   '<div class="col">';
+									echo    '<div class="card shadow-sm">';
+									echo           '<h3>' . $row['tipovehiculo'] . '</h3>';
+									if ($row['estado'] == 0) {
+										echo     '<img src="https://firebasestorage.googleapis.com/v0/b/datos-ceb65.appspot.com/o/New%20Project%20(6).png?alt=media&token=8e603bf2-0532-4bb8-8b6b-8cb7978b20c3" class="rounded float-start" alt="...">';
+										echo     ' <div class="card-body">';
+										echo       '<div class="d-flex justify-content-between align-items-center">';
+										echo         '<div class="btn-group">';
+										echo 			'<a class="btn btn-primary me-2" href="pages-registro-celda.php?id=' . $row['id'] . '">Asignar </a>';
+
+
+										echo          '<h6>CELDA#' . $row['id'] . '</h6>';
+										echo       '</div>';
+										echo        '<small class="text-muted"></small>';
+										echo      '</div>';
+										echo     '</div>';
+										echo   '</div>';
+										echo '</div>';
+									} else {
+										echo     '<img src="https://firebasestorage.googleapis.com/v0/b/datos-ceb65.appspot.com/o/WhatsApp%20Image%202021-11-07%20at%208.58.40%20AM.jpeg?alt=media&token=958d436d-a97a-4a7a-9464-6baddbffe305" class="rounded float-start" alt="...">';
+										echo     ' <div class="card-body">';
+										echo       '<div class="d-flex justify-content-between align-items-center">';
+										echo         '<div class="btn-group">';
+										echo 			'<a class="btn btn-success me-2" href="pages-factura.php?id=' . $row['id'] . '">Factutar</a>';
+
+
+										echo          '<h6>CELDA#' . $row['id'] . '</h6>';
+										echo       '</div>';
+										echo        '<small class="text-muted"></small>';
+										echo      '</div>';
+										echo     '</div>';
+										echo   '</div>';
+										echo '</div>';
+									}
 								}
-								else{
-								echo     '<img src="https://firebasestorage.googleapis.com/v0/b/datos-ceb65.appspot.com/o/WhatsApp%20Image%202021-11-07%20at%208.58.40%20AM.jpeg?alt=media&token=958d436d-a97a-4a7a-9464-6baddbffe305" class="rounded float-start" alt="...">';
-								echo     ' <div class="card-body">';								
-								echo       '<div class="d-flex justify-content-between align-items-center">';
-								echo         '<div class="btn-group">';
-								echo 			'<a class="btn btn-success me-2" href="pages-factura.php?id='.$row['id'].'">Factutar</a>';
+							} else {
+								echo '<div class="col-xl-6 col-xxl-5 d-flex">';
+								echo '<div class="container-fluid p-0">';
+								echo '<h1 class="h3 mb-3"><strong>Autos Colombia Sede: </strong> Medellín</h1>';
 
-								
-								echo          '<h6>CELDA#'. $row['id'] . '</h6>';
-								echo       '</div>';
-								echo        '<small class="text-muted"></small>';
-								echo      '</div>';
-								echo     '</div>';
-								echo   '</div>';
+								echo	'<a class="sidebar-brand" href="index.html">';
+								echo '<span class="align-middle">Autos Colombia</span>';
+								echo '<img src="img\avatars\autologo.png" alt="XXX" class="img-fluid rounded-circle" width="13200" height="13200" />';
+								echo '</a>';
+								echo '<h1 class="h3 mb-3"><strong>Ingresa tus credenciales para acceder a las funciones</strong></h1>';
+								echo '<h4>Aplicacion con fines educativos creada por :</h4>';
+								echo '<h4>Luis Valencia </h4>';
+								echo '<h4>Paola Gallego</h4>';
+								echo '<h4>Kevin Cadavid </h4>';
+								echo '<h4>Damián Perez</h4>';
+								echo '<h4>Jhon Valencia</h4>';
+
+
 								echo '</div>';
-								}
-								
+								echo '</div>';
 							}
 
 
@@ -225,270 +248,83 @@ session_start();
 
 				<div class="container-fluid p-0">
 
-					<h1 class="h3 mb-3"><strong>Analytics</strong> Dashboard</h1>
+					<h1 class="h3 mb-3"><strong>Autos Colombia Sede: </strong> Medellin</h1>
+
 
 					<div class="row">
 						<div class="col-xl-6 col-xxl-5 d-flex">
 							<div class="w-100">
 								<div class="row">
 									<div class="col-sm-6">
-										<div class="card">
-											<div class="card-body">
-												<div class="row">
-													<div class="col mt-0">
-														<h5 class="card-title">Sales</h5>
-													</div>
 
-													<div class="col-auto">
-														<div class="stat text-primary">
-															<i class="align-middle" data-feather="truck"></i>
-														</div>
-													</div>
-												</div>
-												<h1 class="mt-1 mb-3">2.382</h1>
-												<div class="mb-0">
-													<span class="text-danger"> <i class="mdi mdi-arrow-bottom-right"></i> -3.65% </span>
-													<span class="text-muted">Since last week</span>
-												</div>
-											</div>
-										</div>
-										<div class="card">
-											<div class="card-body">
-												<div class="row">
-													<div class="col mt-0">
-														<h5 class="card-title">Visitors</h5>
-													</div>
 
-													<div class="col-auto">
-														<div class="stat text-primary">
-															<i class="align-middle" data-feather="users"></i>
-														</div>
-													</div>
-												</div>
-												<h1 class="mt-1 mb-3">14.212</h1>
-												<div class="mb-0">
-													<span class="text-success"> <i class="mdi mdi-arrow-bottom-right"></i> 5.25% </span>
-													<span class="text-muted">Since last week</span>
-												</div>
-											</div>
-										</div>
 									</div>
 									<div class="col-sm-6">
 										<div class="card">
 											<div class="card-body">
 												<div class="row">
 													<div class="col mt-0">
-														<h5 class="card-title">Earnings</h5>
+														<h5 class="card-title">Medellín</h5>
+														<!-- clock widget start -->
+														<script type="text/javascript">
+															var css_file = document.createElement("link");
+															css_file.setAttribute("rel", "stylesheet");
+															css_file.setAttribute("type", "text/css");
+															css_file.setAttribute("href", "https://s.bookcdn.com//css/cl/bw-cl-c22.css?v=0.0.1");
+															document.getElementsByTagName("head")[0].appendChild(css_file);
+														</script>
+														<div id="tw_22_272007161">
+															<div style="width:200px; height:px; margin: 0 auto;"><a href="https://hotelmix.es/time/medellin-19102">Medellín</a><br /></div>
+														</div>
+														<script type="text/javascript">
+															function setWidgetData_272007161(data) {
+																if (typeof(data) != 'undefined' && data.results.length > 0) {
+																	for (var i = 0; i < data.results.length; ++i) {
+																		var objMainBlock = '';
+																		var params = data.results[i];
+																		objMainBlock = document.getElementById('tw_' + params.widget_type + '_' + params.widget_id);
+																		if (objMainBlock !== null) objMainBlock.innerHTML = params.html_code;
+																	}
+																}
+															}
+															var clock_timer_272007161 = -1;
+															widgetSrc = "https://widgets.booked.net/time/info?ver=2;domid=582;type=22;id=272007161;scode=2;city_id=19102;wlangid=4;mode=1;details=0;background=ffffff;border_color=ffffff;color=686868;add_background=ffffff;add_color=333333;head_color=ffffff;border=0;transparent=0";
+															var widgetUrl = location.href;
+															widgetSrc += '&ref=' + widgetUrl;
+															var wstrackId = "";
+															if (wstrackId) {
+																widgetSrc += ';wstrackId=' + wstrackId + ';'
+															}
+															var timeBookedScript = document.createElement("script");
+															timeBookedScript.setAttribute("type", "text/javascript");
+															timeBookedScript.src = widgetSrc;
+															document.body.appendChild(timeBookedScript);
+														</script>
+														<!-- clock widget end -->
+
 													</div>
 
-													<div class="col-auto">
-														<div class="stat text-primary">
-															<i class="align-middle" data-feather="dollar-sign"></i>
-														</div>
-													</div>
+
 												</div>
-												<h1 class="mt-1 mb-3">$21.300</h1>
+
 												<div class="mb-0">
-													<span class="text-success"> <i class="mdi mdi-arrow-bottom-right"></i> 6.65% </span>
-													<span class="text-muted">Since last week</span>
+
+
 												</div>
 											</div>
 										</div>
 										<div class="card">
-											<div class="card-body">
-												<div class="row">
-													<div class="col mt-0">
-														<h5 class="card-title">Orders</h5>
-													</div>
 
-													<div class="col-auto">
-														<div class="stat text-primary">
-															<i class="align-middle" data-feather="shopping-cart"></i>
-														</div>
-													</div>
-												</div>
-												<h1 class="mt-1 mb-3">64</h1>
-												<div class="mb-0">
-													<span class="text-danger"> <i class="mdi mdi-arrow-bottom-right"></i> -2.25% </span>
-													<span class="text-muted">Since last week</span>
-												</div>
-											</div>
 										</div>
 									</div>
 								</div>
 							</div>
 						</div>
 
-						<div class="col-xl-6 col-xxl-7">
-							<div class="card flex-fill w-100">
-								<div class="card-header">
 
-									<h5 class="card-title mb-0">Recent Movement</h5>
-								</div>
-								<div class="card-body py-3">
-									<div class="chart chart-sm">
-										<canvas id="chartjs-dashboard-line"></canvas>
-									</div>
-								</div>
-							</div>
-						</div>
 					</div>
 
-					<div class="row">
-						<div class="col-12 col-md-6 col-xxl-3 d-flex order-2 order-xxl-3">
-							<div class="card flex-fill w-100">
-								<div class="card-header">
 
-									<h5 class="card-title mb-0">Browser Usage</h5>
-								</div>
-								<div class="card-body d-flex">
-									<div class="align-self-center w-100">
-										<div class="py-3">
-											<div class="chart chart-xs">
-												<canvas id="chartjs-dashboard-pie"></canvas>
-											</div>
-										</div>
-
-										<table class="table mb-0">
-											<tbody>
-												<tr>
-													<td>Chrome</td>
-													<td class="text-end">4306</td>
-												</tr>
-												<tr>
-													<td>Firefox</td>
-													<td class="text-end">3801</td>
-												</tr>
-												<tr>
-													<td>IE</td>
-													<td class="text-end">1689</td>
-												</tr>
-											</tbody>
-										</table>
-									</div>
-								</div>
-							</div>
-						</div>
-						<div class="col-12 col-md-12 col-xxl-6 d-flex order-3 order-xxl-2">
-							<div class="card flex-fill w-100">
-								<div class="card-header">
-
-									<h5 class="card-title mb-0">Real-Time</h5>
-								</div>
-								<div class="card-body px-4">
-									<div id="world_map" style="height:350px;"></div>
-								</div>
-							</div>
-						</div>
-						<div class="col-12 col-md-6 col-xxl-3 d-flex order-1 order-xxl-1">
-							<div class="card flex-fill">
-								<div class="card-header">
-
-									<h5 class="card-title mb-0">Calendar</h5>
-								</div>
-								<div class="card-body d-flex">
-									<div class="align-self-center w-100">
-										<div class="chart">
-											<div id="datetimepicker-dashboard"></div>
-										</div>
-									</div>
-								</div>
-							</div>
-						</div>
-					</div>
-
-					<div class="row">
-						<div class="col-12 col-lg-8 col-xxl-9 d-flex">
-							<div class="card flex-fill">
-								<div class="card-header">
-
-									<h5 class="card-title mb-0">Latest Projects</h5>
-								</div>
-								<table class="table table-hover my-0">
-									<thead>
-										<tr>
-											<th>Name</th>
-											<th class="d-none d-xl-table-cell">Start Date</th>
-											<th class="d-none d-xl-table-cell">End Date</th>
-											<th>Status</th>
-											<th class="d-none d-md-table-cell">Assignee</th>
-										</tr>
-									</thead>
-									<tbody>
-										<tr>
-											<td>Project Apollo</td>
-											<td class="d-none d-xl-table-cell">01/01/2021</td>
-											<td class="d-none d-xl-table-cell">31/06/2021</td>
-											<td><span class="badge bg-success">Done</span></td>
-											<td class="d-none d-md-table-cell">Vanessa Tucker</td>
-										</tr>
-										<tr>
-											<td>Project Fireball</td>
-											<td class="d-none d-xl-table-cell">01/01/2021</td>
-											<td class="d-none d-xl-table-cell">31/06/2021</td>
-											<td><span class="badge bg-danger">Cancelled</span></td>
-											<td class="d-none d-md-table-cell">William Harris</td>
-										</tr>
-										<tr>
-											<td>Project Hades</td>
-											<td class="d-none d-xl-table-cell">01/01/2021</td>
-											<td class="d-none d-xl-table-cell">31/06/2021</td>
-											<td><span class="badge bg-success">Done</span></td>
-											<td class="d-none d-md-table-cell">Sharon Lessman</td>
-										</tr>
-										<tr>
-											<td>Project Nitro</td>
-											<td class="d-none d-xl-table-cell">01/01/2021</td>
-											<td class="d-none d-xl-table-cell">31/06/2021</td>
-											<td><span class="badge bg-warning">In progress</span></td>
-											<td class="d-none d-md-table-cell">Vanessa Tucker</td>
-										</tr>
-										<tr>
-											<td>Project Phoenix</td>
-											<td class="d-none d-xl-table-cell">01/01/2021</td>
-											<td class="d-none d-xl-table-cell">31/06/2021</td>
-											<td><span class="badge bg-success">Done</span></td>
-											<td class="d-none d-md-table-cell">William Harris</td>
-										</tr>
-										<tr>
-											<td>Project X</td>
-											<td class="d-none d-xl-table-cell">01/01/2021</td>
-											<td class="d-none d-xl-table-cell">31/06/2021</td>
-											<td><span class="badge bg-success">Done</span></td>
-											<td class="d-none d-md-table-cell">Sharon Lessman</td>
-										</tr>
-										<tr>
-											<td>Project Romeo</td>
-											<td class="d-none d-xl-table-cell">01/01/2021</td>
-											<td class="d-none d-xl-table-cell">31/06/2021</td>
-											<td><span class="badge bg-success">Done</span></td>
-											<td class="d-none d-md-table-cell">Christina Mason</td>
-										</tr>
-										<tr>
-											<td>Project Wombat</td>
-											<td class="d-none d-xl-table-cell">01/01/2021</td>
-											<td class="d-none d-xl-table-cell">31/06/2021</td>
-											<td><span class="badge bg-warning">In progress</span></td>
-											<td class="d-none d-md-table-cell">William Harris</td>
-										</tr>
-									</tbody>
-								</table>
-							</div>
-						</div>
-						<div class="col-12 col-lg-4 col-xxl-3 d-flex">
-							<div class="card flex-fill w-100">
-								<div class="card-header">
-
-									<h5 class="card-title mb-0">Monthly Sales</h5>
-								</div>
-								<div class="card-body d-flex w-100">
-									<div class="align-self-center chart chart-lg">
-										<canvas id="chartjs-dashboard-bar"></canvas>
-									</div>
-								</div>
-							</div>
-						</div>
-					</div>
 
 				</div>
 			</main>
@@ -498,22 +334,22 @@ session_start();
 					<div class="row text-muted">
 						<div class="col-6 text-start">
 							<p class="mb-0">
-								<a class="text-muted" href="https://adminkit.io/" target="_blank"><strong>AdminKit</strong></a> &copy;
+								<a class="text-muted" href="https://adminkit.io/" target="_blank"><strong>IUDigital2021</strong></a> &copy;
 							</p>
 						</div>
 						<div class="col-6 text-end">
 							<ul class="list-inline">
 								<li class="list-inline-item">
-									<a class="text-muted" href="https://adminkit.io/" target="_blank">Support</a>
+									<a class="text-muted" href="https://adminkit.io/" target="_blank">Soporte</a>
 								</li>
 								<li class="list-inline-item">
-									<a class="text-muted" href="https://adminkit.io/" target="_blank">Help Center</a>
+									<a class="text-muted" href="https://adminkit.io/" target="_blank">Centro de Ayudas</a>
 								</li>
 								<li class="list-inline-item">
-									<a class="text-muted" href="https://adminkit.io/" target="_blank">Privacy</a>
+									<a class="text-muted" href="https://adminkit.io/" target="_blank">Seguridad</a>
 								</li>
 								<li class="list-inline-item">
-									<a class="text-muted" href="https://adminkit.io/" target="_blank">Terms</a>
+									<a class="text-muted" href="https://adminkit.io/" target="_blank">Terminos</a>
 								</li>
 							</ul>
 						</div>
